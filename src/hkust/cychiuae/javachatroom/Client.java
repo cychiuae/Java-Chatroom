042b131fcdb1e3b2b1bc1da7c71c95c37e847051
cychiuae <cychiuae@connect.ust.hk>
2015-05-16 02:43:53 +0800

    updated client window

--- src/hkust/cychiuae/javachatroom/Client.java
+++ src/hkust/cychiuae/javachatroom/Client.java
@@ -1,13 +1,15 @@
 package hkust.cychiuae.javachatroom;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
 
 public class Client extends JFrame {
+
+	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 	
@@ -25,12 +27,26 @@ public class Client extends JFrame {
 	
 	private void createWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(850, 550);
+		setSize(880, 550);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{16, 857, 7};  //sum = 880
+		gbl_contentPane.rowHeights = new int[]{35, 475, 40};  // sum = 550
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea txtrHistory = new JTextArea();
+		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
+		gbc_txtrHistory.gridx = 1;
+		gbc_txtrHistory.gridy = 1;
+		contentPane.add(txtrHistory, gbc_txtrHistory);
+		
 		setVisible(true);
 	}
 
